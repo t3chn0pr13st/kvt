efcb223a41cb931498e39243b27c572c6f6dfc56
Kvalood <x404@bk.ru>
2022-05-24 14:10:07 +1000

    fix load ext

--- js/content.js
+++ js/content.js
@@ -1,50 +1,16 @@
 'use strict';
 
-let versionApi = "2.0.0",
-    settings = {};
-
-function getCookies(e) {
-	e = RegExp(e + "[^;]+").exec(document.cookie);
-	return decodeURIComponent(e ? e.toString().replace(/^[^=]+./, "") : "")
-}
-
-function getPsid() {
-	return getCookies("psid")
-}
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request && request.type === "config") {
-        sendResponse({msg: "config", psid: getPsid(), versionApi: versionApi});
-    }
-});
-
-
-chrome.storage.local.get('compactStyle', (result) => {
-    if(result['compactStyle']) {
-        setTimeout(function () {
-            document.getElementById("root").classList.add("kvt-compactStyle");
-        }, 2000)
-    }
-});
-
-['kvtFastVolumePrice', 'kvtFastVolumePriceRound', 'kvtFastVolumeSize', 'kvtSTIGFastVolSumBot', 'kvtSTIGFastVolSumRcktMon', 'telegramId', 'rcktMonConnect', 'alorToken', 'IsShortTicker'].forEach(function (st) {
-    chrome.storage.local.get(st, (result) => {
-        settings[st] = result[st]
-    })
-})
-
-setTimeout(function () {
-    let tagName = document.getElementsByTagName('body')[0],
-        el = document.createElement("div");
-
-    settings['extensionId'] = chrome.runtime.id
-    settings['extensionVer'] = chrome.runtime.getManifest().version
-
-    el.setAttribute("data-kvt-extension", "");
-    el.textContent = JSON.stringify(settings)
-    tagName.appendChild(el)
-});
-
+const getObjectFromLocalStorage = async function (key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function (value) {
+                resolve(value[key]);
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
 
 function injectScript(url, tag, setExtId) {
     var tagName = document.getElementsByTagName(tag)[0],
@@ -55,9 +21,50 @@ function injectScript(url, tag, setExtId) {
     tagName.appendChild(el)
 }
 
-injectScript(chrome.runtime.getURL("js/helpers.js?t=" + Date.now()), "body")
-injectScript(chrome.runtime.getURL("js/alor.js?t=" + Date.now()), "body")
+function getCookies(e) {
+    e = RegExp(e + "[^;]+").exec(document.cookie);
+    return decodeURIComponent(e ? e.toString().replace(/^[^=]+./, "") : "")
+}
 
-setTimeout(function () {
+function getPsid() {
+    return getCookies("psid")
+}
+
+async function run() {
+
+    let versionApi = "2.0.0",
+        settings = {};
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request && request.type === "config") {
+            sendResponse({msg: "config", psid: getPsid(), versionApi: versionApi});
+        }
+    });
+
+    if (await getObjectFromLocalStorage("compactStyle")) {
+        document.getElementById("root").classList.add("kvt-compactStyle");
+    }
+
+    for (const key of ['kvtFastVolumePrice', 'kvtFastVolumePriceRound', 'kvtFastVolumeSize', 'kvtSTIGFastVolSumBot', 'kvtSTIGFastVolSumRcktMon', 'telegramId', 'rcktMonConnect', 'alorToken', 'IsShortTicker']) {
+        settings[key] = await getObjectFromLocalStorage(key)
+    }
+
+    // insert settings in html
+    setTimeout(function () {
+        let tagName = document.getElementsByTagName('body')[0],
+            el = document.createElement("div");
+
+        settings['extensionId'] = chrome.runtime.id
+        settings['extensionVer'] = chrome.runtime.getManifest().version
+
+        el.setAttribute("data-kvt-extension", "");
+        el.textContent = JSON.stringify(settings)
+        tagName.appendChild(el)
+    });
+
+    injectScript(chrome.runtime.getURL("js/helpers.js?t=" + Date.now()), "body")
+    injectScript(chrome.runtime.getURL("js/alor.js?t=" + Date.now()), "body")
     injectScript(chrome.runtime.getURL("js/page.js?t=" + Date.now()), "body")
-});
+}
+
+run()
